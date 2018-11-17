bebc0dfec9f8c6f1aafc0b98ae2a2985f89d34fe
DaniTur <daniturzurita@gmail.com>
2018-11-17 15:32:51 +0100

    Add Contadores de FPS y Actualizaciones por segundo

--- src/Juego/Juego.java
+++ src/Juego/Juego.java
@@ -14,7 +14,12 @@ public class Juego extends Canvas implements Runnable {
 	private static JFrame ventana;
 	private static final int ANCHO = 800;
 	private static final int ALTO = 600;
+
 	private static final String NOMBRE = "Juego";
+
+	private static int aps = 0;
+	private static int fps = 0;
+
 	private static Thread thread;
 
 	// volatile, no se puede usar esta variable a la vez en diferentes threads
@@ -63,10 +68,51 @@ public class Juego extends Canvas implements Runnable {
 		}
 	}
 
+	private void actualizar() {
+		aps++; // contador de actualizaciones del programa por segundo
+	}
+
+	private void mostrar() {
+		fps++; // contador de fps
+	}
+
 	@Override
 	public void run() {
-		while (enFuncionamiento) {
+		final int NS_POR_SEGUNDO = 1000000000; // cuantos nanosegundos hay en 1 segundo
+		final byte APS_OBJECTIVO = 60; // cuantas actualizaciones por segundo queremos
+		final double NS_POR_ACTUALIZACION = NS_POR_SEGUNDO / APS_OBJECTIVO; // 1 segundo(en nanosegundos) divido por las
+																			// actualizaciones que queremos(60)
+		// referencias para controlar velocidad del programa
+		long referenciaActualizacion = System.nanoTime();
+		long referenciaContador = System.nanoTime();
 
+		double tiempoTranscurrido;
+		double delta = 0; // delta: cantidad de tiempo que ocurre hasta que hay una actualizacion
+
+		while (enFuncionamiento) {
+			final long inicioBucle = System.nanoTime(); // le fijamos unos nanosegundos cuando se ha iniciado el bucle
+
+			tiempoTranscurrido = inicioBucle - referenciaActualizacion; // cuanto tiempo ha pasado entre la referencia y
+																		// el inicio del bucle
+			referenciaActualizacion = inicioBucle;
+
+			delta += tiempoTranscurrido / NS_POR_ACTUALIZACION;
+
+			while (delta >= 1) { // limitamos y controlamos la velocidad de actualizacion(60 veces por segundo)
+				actualizar();
+				delta--;
+			}
+
+			mostrar();
+
+			// contador se actualiza cada segundo y lo mostramos en la ventana
+			if (System.nanoTime() - referenciaContador > NS_POR_SEGUNDO) {
+				ventana.setTitle(NOMBRE + "  APS: " + aps + "  FPS: " + fps);
+
+				aps = 0; // reiniciamos contadores
+				fps = 0;
+				referenciaContador = System.nanoTime();
+			}
 		}
 	}
 
