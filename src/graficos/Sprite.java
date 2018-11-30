37273a5e7fedc76bf5a726791c1bbb02994f709a
DaniTur <daniturzurita@gmail.com>
2018-11-30 15:44:10 +0100

    Change Sprite method cargaNormal, cargaSrptie and a version parameter

--- src/graficos/Sprite.java
+++ src/graficos/Sprite.java
@@ -12,22 +12,22 @@ public final class Sprite {
 
 	// coleccion de sprites
 	public static final Sprite VACIO = new Sprite(32, 0);
-	public static final Sprite ASFALTO = new Sprite(32, 0, 0, HojaSprites.desierto);
-	public static final Sprite ARENA = new Sprite(32, 1, 0, HojaSprites.desierto);
-	public static final Sprite BORDE_CARRETERA = new Sprite(32, 2, 0, HojaSprites.desierto);
-	public static final Sprite CENTRO_CARRETERA = new Sprite(32, 3, 0, HojaSprites.desierto);
-	public static final Sprite ESQUINA_CARRETERA = new Sprite(32, 4, 0, HojaSprites.desierto);
-	public static final Sprite PARED_PIEDRA = new Sprite(32, 5, 0, HojaSprites.desierto);
-	public static final Sprite PARED_PIEDRA_INFERIOR = new Sprite(32, 6, 0, HojaSprites.desierto);
-	public static final Sprite PARED_PIEDRA_CARRETERA = new Sprite(32, 0, 3, HojaSprites.desierto);
-	public static final Sprite PUERTA_SUPERIOR_IZQUIERDA = new Sprite(32, 7, 0, HojaSprites.desierto);
-	public static final Sprite PUERTA_INTERMEDIA_IZQUIERDA = new Sprite(32, 7, 1, HojaSprites.desierto);
-	public static final Sprite PUERTA_INFERIOR = new Sprite(32, 8, 1, HojaSprites.desierto);
-	public static final Sprite OXIDO = new Sprite(32, 9, 0, HojaSprites.desierto);
-	public static final Sprite PUERTA_SUPERIOR_CENTRAL = new Sprite(32, 8, 0, HojaSprites.desierto);
+	public static final Sprite ASFALTO = new Sprite(32, 0, 0, 0, HojaSprites.desierto);
+	public static final Sprite ARENA = new Sprite(32, 1, 0, 0, HojaSprites.desierto);
+	public static final Sprite BORDE_CARRETERA = new Sprite(32, 2, 0, 0, HojaSprites.desierto);
+	public static final Sprite CENTRO_CARRETERA = new Sprite(32, 3, 0, 0, HojaSprites.desierto);
+	public static final Sprite ESQUINA_CARRETERA = new Sprite(32, 4, 0, 0, HojaSprites.desierto);
+	public static final Sprite PARED_PIEDRA = new Sprite(32, 5, 0, 0, HojaSprites.desierto);
+	public static final Sprite PARED_PIEDRA_INFERIOR = new Sprite(32, 6, 0, 0, HojaSprites.desierto);
+	public static final Sprite PARED_PIEDRA_CARRETERA = new Sprite(32, 0, 3, 0, HojaSprites.desierto);
+	public static final Sprite PUERTA_SUPERIOR_IZQUIERDA = new Sprite(32, 7, 0, 0, HojaSprites.desierto);
+	public static final Sprite PUERTA_INTERMEDIA_IZQUIERDA = new Sprite(32, 7, 1, 0, HojaSprites.desierto);
+	public static final Sprite PUERTA_INFERIOR = new Sprite(32, 8, 1, 0, HojaSprites.desierto);
+	public static final Sprite OXIDO = new Sprite(32, 9, 0, 0, HojaSprites.desierto);
+	public static final Sprite PUERTA_SUPERIOR_CENTRAL = new Sprite(32, 8, 0, 0, HojaSprites.desierto);
 	// fin de la coleccion
 
-	public Sprite(final int lado, final int columna, final int fila, final HojaSprites hoja) {
+	public Sprite(final int lado, final int columna, final int fila, int version, final HojaSprites hoja) {
 		this.lado = lado;
 
 		pixeles = new int[lado * lado];
@@ -36,11 +36,7 @@ public final class Sprite {
 		this.y = fila * lado;
 		this.hoja = hoja;
 
-		for (int y = 0; y < lado; y++) {
-			for (int x = 0; x < lado; x++) {
-				pixeles[x + y * lado] = hoja.pixeles[(x + this.x) + (y + this.y) * hoja.obtenAncho()];
-			}
-		}
+		cargarSprite(version);
 	}
 
 	public Sprite(final int lado, final int color) {
@@ -54,5 +50,21 @@ public final class Sprite {
 
 	public int obtenLado() {
 		return lado;
+	}
+
+	private void cargarSprite(int version) {
+		if (version == 0) {
+			cargaNormal();
+		} else {
+
+		}
+	}
+
+	private void cargaNormal() {
+		for (int y = 0; y < lado; y++) {
+			for (int x = 0; x < lado; x++) {
+				pixeles[x + y * lado] = hoja.pixeles[(x + this.x) + (y + this.y) * hoja.obtenAncho()];
+			}
+		}
 	}
 }
