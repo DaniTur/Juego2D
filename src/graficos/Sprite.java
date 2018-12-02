b367f4aad3e11838a794d4b94bc6ff9a2ddb2887
DaniTur <daniturzurita@gmail.com>
2018-12-02 15:53:08 +0100

    Add metodos para rotar e invertir sprites para las 8 veriones

--- src/graficos/Sprite.java
+++ src/graficos/Sprite.java
@@ -56,7 +56,7 @@ public final class Sprite {
 		if (version == 0) {
 			cargaNormal();
 		} else {
-
+			cargaManipulada(version);
 		}
 	}
 
@@ -64,6 +64,124 @@ public final class Sprite {
 		for (int y = 0; y < lado; y++) {
 			for (int x = 0; x < lado; x++) {
 				pixeles[x + y * lado] = hoja.pixeles[(x + this.x) + (y + this.y) * hoja.obtenAncho()];
+			}
+		}
+	}
+
+	private void cargaManipulada(int version) { // pasandole la verion que queremos del sprite y una copia de sus
+												// pixeles manipularemos la manera de cargar los pixeles para rotar los
+												// sprites
+		int[] pixelesTemporales = iniciarPixelesTemporales();
+
+		// dependiedo de la version rotamos los sprites
+		switch (version) {
+		case 1:
+			invertirX(pixelesTemporales);
+			break;
+		case 2:
+			invertirY(pixelesTemporales);
+			break;
+		case 3:
+			invertirXY(pixelesTemporales);
+			break;
+		case 4:
+			rotar90I(pixelesTemporales);
+			break;
+		case 5:
+			rotar90D(pixelesTemporales);
+			break;
+		case 6:
+			rotarI90InvertirY(pixelesTemporales);
+			break;
+		case 7:
+			rotarD90InvertirY(pixelesTemporales);
+			break;
+		default:
+			cargaNormal();
+		}
+
+	}
+
+	private int[] iniciarPixelesTemporales() { // hacemos una copia de los pixeles de un sprite temporalmente para
+												// rotarlos
+		int[] pixelesTemporales = new int[lado * lado];
+		for (int y = 0; y < lado; y++) {
+			for (int x = 0; x < lado; x++) {
+				pixelesTemporales[x + y * lado] = hoja.pixeles[(x + this.x) + (y + this.y) * hoja.obtenAncho()];
+			}
+		}
+		return pixelesTemporales;
+	}
+
+	// 1
+	private void invertirX(int[] pixelesTemporales) { // le pasamos la copia del sprite temporal
+		int i = 0;
+		for (int y = 0; y < lado; y++) {
+			for (int x = lado - 1; x > -1; x--) {
+				pixeles[i] = pixelesTemporales[x + y * lado];
+				i++;
+			}
+		}
+	}
+
+	// 2
+	private void invertirY(int[] pixelesTemporales) {
+		int i = 0;
+		for (int y = lado - 1; y > -1; y--) {
+			for (int x = 0; x < lado; x++) {
+				pixeles[i] = pixelesTemporales[x + y * lado];
+				i++;
+			}
+		}
+	}
+
+	// 3
+	private void invertirXY(int[] pixelesTemporales) {
+		for (int i = 0; i < pixeles.length; i++) {
+			pixeles[i] = pixelesTemporales[pixelesTemporales.length - 1 - i];
+		}
+	}
+
+	// 4
+	private void rotar90I(int[] pixelesTemporales) {
+		int i = 0;
+		for (int x = lado - 1; x > -1; x--) {
+			for (int y = 0; y < lado; y++) {
+				pixeles[i] = pixelesTemporales[x + y * lado];
+				i++;
+			}
+		}
+	}
+
+	// 5
+	private void rotar90D(int[] pixelesTemporales) {
+		int i = 0;
+		for (int x = 0; x < lado; x++) {
+			for (int y = lado - 1; y > -1; y--) {
+				pixeles[i] = pixelesTemporales[x + y * lado];
+				i++;
+			}
+		}
+	}
+
+	// 6
+	private void rotarI90InvertirY(int[] pixelesTemporales) {
+		int i = 0;
+		for (int x = 0; x < lado; x++) {
+			for (int y = 0; y < lado; y++) {
+				pixeles[i] = pixelesTemporales[x + y * lado];
+				i++;
+			}
+		}
+	}
+
+	// 7
+	private void rotarD90InvertirY(int[] pixelesTemporales) {
+		int i = 0;
+		for (int x = lado - 1; x > -1; x--) {
+			for (int y = lado - 1; y > -1; y--) {
+				pixeles[i] = pixelesTemporales[x + y * lado];
+				i++;
 			}
 		}
 	}
